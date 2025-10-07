0b976823f1087ca99a6dcdd40912dc1b18ff1b21
ViaceslaVV <160885897+ViaceslaVV@users.noreply.github.com>
2025-10-07 16:51:13 +0300

    defgsf
    gdbcbvnh

--- Assets/Scripts/ObstacleSpawner.cs
+++ Assets/Scripts/ObstacleSpawner.cs
@@ -1,33 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UIElements;
 
 public class ObstacleSpawner : MonoBehaviour
 {
     public float cooldownMin = 1;
     public float cooldownMax = 3;
 
-    public float xOffsetMin = 2;
-    public float xOffsetMax = 4;
+    public float safeSize = 1.5f;
 
     public GameObject obstaclePrefab;
 
+    float safeLocation;
     float lastSpawnTime = 1;
     Vector3 positionOffset = new();
-    Vector2 scale;
+    Transform[] transforms;
 
     void Start()
     {
-        positionOffset.y = obstaclePrefab.transform.position.y;
-        positionOffset.x = Random.Range(xOffsetMin, xOffsetMax) + 9;
+        positionOffset.x = 9;
 
-        scale.x = 1;
-        scale.y = 1;
+
+
+        foreach (Transform t in obstaclePrefab.transform)
+        {
+            transforms.Append<Transform>(t);
+        }
     }
-
+    
     void Update()
     {
-        if (lastSpawnTime <= 0)
+        if(lastSpawnTime<= 0)
+        {
+            safeLocation = Random.Range(-3.5f, 4f);
+
+            transforms[0].localScale = Vector3.down * (positionOffset.y + safeSize / 2);
+            transforms[1].localScale = Vector3.up * (positionOffset.y - safeSize / 2);
+
+            positionOffset.y = 4f - transforms[0].localScale.y / 2;
+            positionOffset.y = 3.5f + transforms[1].localScale.y / 2;
+
+            Instantiate(obstaclePrefab, positionOffset, Quaternion.identity);
+
+            lastSpawnTime = lastSpawnTime-Time.deltaTime * Random.Range(1f, 1.5f);
+        }
+
+
+        /*if (lastSpawnTime <= 0)
         {
             scale.y = Random.Range(1, 3);
             if(positionOffset.y > 0)
@@ -42,6 +63,6 @@ public class ObstacleSpawner : MonoBehaviour
             Instantiate(obstaclePrefab, positionOffset, Quaternion.identity);
             lastSpawnTime = Random.Range(cooldownMin, cooldownMax);
         }
-        lastSpawnTime -= Time.deltaTime;
+        lastSpawnTime -= Time.deltaTime;*/
     }
 }
